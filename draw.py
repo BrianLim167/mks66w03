dba3d2cb07b927d925c30039b019ad0b610940e6
BrianLim167 <blim167@gmail.com>
2018-02-15 10:29:48 -0500

    updated sauce

--- draw.py
+++ draw.py
@@ -1,6 +1,9 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
+    if ( x1 < x0 ):
+        (x0, x1) = (x1, x0)
+        (y0, y1) = (y1, y0)
     a = y1 - y0
     b = x0 - x1
     (x,y) = (x0,y0)
@@ -34,12 +37,13 @@ def draw_line( x0, y0, x1, y1, screen, color ):
             x += 1
             d += 2*a
         return
-##    if ( -b <= a ):
-##        d = a + 2*b
-##        while ( y <= y1 ):
-##            plot(screen, color, x, y)
-##            if ( d < 0 ):
-##                x += 1
-##                d += 2*a
-##            y += 1
-##            d += 2*b
+    if ( a <= b ):
+        d = a - 2*b
+        while ( y >= y1 ):
+            plot(screen, color, x, y)
+            if ( d > 0 ):
+                x += 1
+                d += 2*a
+            y -= 1
+            d -= 2*b
+        return
--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 from display import *
 from draw import *
+import math
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
 
-(x0,y0) = (120,240)
-(x1,y1) = (200,200)
+##green = [0,255,0]
+##draw_line(250,250, 250+150,250+50, screen, green)
 
-draw_line(x0,y0, x1,y1, screen, color)
+length = 240
+for i in range(360):
+    h = i / 60
+    c = int(i % 60 * 255 / 60)
+    if (0 <= h < 1):
+        color = [255,c,0]
+    if (1 <= h < 2):
+        color = [255-c,255,0]
+    if (2 <= h < 3):
+        color = [0,255,c]
+    if (3 <= h < 4):
+        color = [0,255-c,255]
+    if (4 <= h < 5):
+        color = [c,0,255]
+    if (5 <= h < 6):
+        color = [255,0,255-c]
+    dx = int(length*math.cos(math.radians(i)))
+    dy = int(length*math.sin(math.radians(i)))
+    draw_line(250,250, 250+dx,250+dy, screen, color)
 
 display(screen)
 save_extension(screen, 'img.png')
